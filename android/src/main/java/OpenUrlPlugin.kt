8671fd1a9c185c8b5461a1d753681121d5ae27b5
giripriyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2024-04-04 14:21:58 +0530

    following updated docs implementation

--- android/src/main/java/OpenUrlPlugin.kt
+++ android/src/main/java/OpenUrlPlugin.kt
@@ -2,6 +2,7 @@ package com.giripriyadarshan.openurl
 
 import android.app.Activity
 import android.content.Intent
+import android.webkit.WebView
 import android.net.Uri
 import androidx.activity.result.ActivityResult
 import app.tauri.annotation.ActivityCallback
@@ -10,15 +11,20 @@ import app.tauri.annotation.InvokeArg
 import app.tauri.annotation.TauriPlugin
 import app.tauri.plugin.Invoke
 import app.tauri.plugin.Plugin
-import app.tauri.plugin.JSObject
+
+
+@InvokeArg
+internal class OpenUrlArgs {
+  lateinit var Url: String
+}
 
 @TauriPlugin
 class OpenUrlPlugin(private val activity: Activity) : Plugin(activity) {
 
     @Command
     fun openUrl(invoke: Invoke) {
-        val url = invoke.getString("url", "")
-        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url.value ?: "default value :("))
+        val url = invoke.parseArgs(OpenUrlArgs::class.java)
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url.Url)
 
         startActivityForResult(invoke, browserIntent, "openUrlResult")
     }
